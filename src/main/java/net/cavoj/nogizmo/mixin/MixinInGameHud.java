04643c28b963485de0717e32b7258f35b5fa7c5e
Samuel Čavoj <samuel@cavoj.net>
2023-09-30 23:55:01 +0100

    Update to 1.20.2

--- src/main/java/net/cavoj/nogizmo/mixin/MixinInGameHud.java
+++ src/main/java/net/cavoj/nogizmo/mixin/MixinInGameHud.java
@@ -1,16 +1,18 @@
 package net.cavoj.nogizmo.mixin;
 
+import net.minecraft.client.gui.hud.DebugHud;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.option.GameOptions;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InGameHud.class)
 public class MixinInGameHud {
-    @Redirect(method = "renderCrosshair", at = @At(value = "FIELD", target = "Lnet/minecraft/client/option/GameOptions;debugEnabled:Z", opcode = Opcodes.GETFIELD))
-    private boolean mixinRenderCrosshairDebug(final GameOptions options) {
+    @Redirect(method = "renderCrosshair", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/DebugHud;shouldShowDebugHud()Z"))
+    private boolean mixinRenderCrosshairDebug(DebugHud instance) {
         return false;
     }
 }
